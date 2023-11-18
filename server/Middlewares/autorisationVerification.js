8d6a2e3d743fce730f25a0071d628fc1737c3a00
Abdo <abdelkoddousmounir@gmail.com>
2023-11-18 23:03:09 +0100

    simple modification

--- server/Middlewares/autorisationVerification.js
+++ server/Middlewares/autorisationVerification.js
@@ -2,17 +2,27 @@ const jwt = require("jsonwebtoken");
 
 // Verify autorisation
 module.exports.authVerification = (req, res, next) => {
-    try {
-        const token = req.headers.accesstoken;
-        if (!token) {
-            return res.status(400).json({ msg: "Token not found, login first" });
+    const token = req.headers.accesstoken;
+
+    if (!token) {
+        return res.status(401).json({ error: 'Unauthorized: Missing token' });
+    }
+
+    jwt.verify(token, process.env.AccessToken, (err, user) => {
+        if (err) {
+            if (err.name === 'TokenExpiredError') {
+                return res.status(401).json({ error: 'Unauthorized: Token expired' });
+            }
+            return res.status(403).json({ error: 'Forbidden: Invalid token' });
+        }
+        
+        // Check if the user ID from the token matches the requested user ID in params or body
+        if (!req.params.id && req.params.id !== user.userId) {
+            return res.status(403).json({ error: 'Forbidden: Access denied' });
         }
 
-        const decoded = jwt.verify(token, process.env.AccessToken);
-        req.userId = decoded._id
+        // user is authenticated and authorized, proceed with the request
+        req.user = user;
         next();
-    } catch (error) {
-        console.error(error);
-        return res.status(401).json({ msg: "Token is not valid, authorization denied" });
-    }
+    });
 };
